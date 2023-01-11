dbfbfa63d125051cae75e2b4df47f9800f3e65e9
ririsisi <278872032@qq.com>
2023-01-11 13:48:30 +0800

    创建SpringBoot项目

---
+++ src/main/java/com/axjd/axjd/AxjdApplication.java
@@ -0,0 +1,13 @@
+package com.axjd.axjd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxjdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxjdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axjd/axjd/AxjdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axjd.axjd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxjdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
