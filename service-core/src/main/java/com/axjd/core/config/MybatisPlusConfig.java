6ae9ca84eb86e421445e181757e87d60583d4c78
ririsisi <278872032@qq.com>
2023-01-11 15:39:51 +0800

    创建MybatisPlus配置类

---
+++ service-core/src/main/java/com/axjd/core/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.axjd.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author: songxq
+ * @Date: 2023/1/11
+ * @Description: com.axjd.core.config
+ * @Version: 1.0
+ */
+@Configuration
+@MapperScan("com.axjd.core.mapper")
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
