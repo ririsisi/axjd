20c20b65acf2cdcc3b8b14eb584507c8d104a55f
ririsisi <QQnn2791996@>
2023-01-23 09:23:27 +0800

    新增积分等级管理列表查询接口

---
+++ service-core/src/main/java/com/axjd/core/controller/admin/AdminIntegralGradeController.java
@@ -0,0 +1,31 @@
+package com.axjd.core.controller.admin;
+
+import com.axjd.core.pojo.entity.IntegralGrade;
+import com.axjd.core.service.IntegralGradeService;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/admin/core/integralGrade")
+@ApiModel("积分等级管理")
+public class AdminIntegralGradeController {
+
+    @Resource
+    private IntegralGradeService integralGradeService;
+
+    @GetMapping("/list")
+    @ApiOperation("积分等级列表")
+    public List<IntegralGrade> listAll() {
+        return integralGradeService.list();
+    }
+
+}
